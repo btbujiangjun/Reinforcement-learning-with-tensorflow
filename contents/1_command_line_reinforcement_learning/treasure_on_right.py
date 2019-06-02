5add7341b20e0391df3bca5b41690058c2efa680
btbujiangjun <jiangjun4@sina.com>
2019-06-02 20:34:02 +0800

    Update treasure_on_right.py

--- contents/1_command_line_reinforcement_learning/treasure_on_right.py
+++ contents/1_command_line_reinforcement_learning/treasure_on_right.py
@@ -23,6 +23,7 @@ FRESH_TIME = 0.3    # fresh time for one move
 
 
 def build_q_table(n_states, actions):
+    #构建q_table, rows为步长，columns为动作
     table = pd.DataFrame(
         np.zeros((n_states, len(actions))),     # q_table initial values
         columns=actions,    # actions's name
@@ -33,7 +34,8 @@ def build_q_table(n_states, actions):
 
 def choose_action(state, q_table):
     # This is how to choose an action
-    state_actions = q_table.iloc[state, :]
+    state_actions = q_table.iloc[state, :] #获取当前步的各个q值
+    #按策略选择行动,取最大值并一定概率随机探索
     if (np.random.uniform() > EPSILON) or ((state_actions == 0).all()):  # act non-greedy or state-action have no value
         action_name = np.random.choice(ACTIONS)
     else:   # act greedy
@@ -42,18 +44,20 @@ def choose_action(state, q_table):
 
 
 def get_env_feedback(S, A):
+    # S 为当前状态 , S_ 为下一状态
     # This is how agent will interact with the environment
+    # 到达终点时reward为1，否则为0
     if A == 'right':    # move right
-        if S == N_STATES - 2:   # terminate
+        if S == N_STATES - 2:   # terminate 当前为倒数第二次，且向右走，则达到终点，下一状态S_ 为 terminal, reward 1.
             S_ = 'terminal'
             R = 1
-        else:
+        else: #还未到达终点，更新S_(这里的state其实就是q_table的当前步骤的索引)
             S_ = S + 1
             R = 0
     else:   # move left
         R = 0
         if S == 0:
-            S_ = S  # reach the wall
+            S_ = S  # duang, reach the wall
         else:
             S_ = S - 1
     return S_, R
@@ -86,13 +90,14 @@ def rl():
 
             A = choose_action(S, q_table)
             S_, R = get_env_feedback(S, A)  # take action & get next state and reward
-            q_predict = q_table.loc[S, A]
+            q_predict = q_table.loc[S, A] # 当前步的q value
             if S_ != 'terminal':
+                # 当前action的reward + 下一步的最优q_value(按GAMMA打折)
                 q_target = R + GAMMA * q_table.iloc[S_, :].max()   # next state is not terminal
             else:
                 q_target = R     # next state is terminal
                 is_terminated = True    # terminate this episode
-
+            # 更新当前步当前行动的q value += 打折的(当前行动的q_predict（当前action reward + 打折的下一步潜在最优q_value）- 当前行动q_value)
             q_table.loc[S, A] += ALPHA * (q_target - q_predict)  # update
             S = S_  # move to next state
 
